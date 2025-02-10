91c4671673ccc9d6e2cc14e47a841ef3ed86e863
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-02-10 17:24:42 +0200

    Update ./com/star4droid/star2d/Helpers/PropertySet.java

--- app/src/main/java/com/star4droid/star2d/Helpers/PropertySet.java
+++ app/src/main/java/com/star4droid/star2d/Helpers/PropertySet.java
@@ -1,15 +1,14 @@
 package com.star4droid.star2d.Helpers;
 
-import android.graphics.Color;
-import android.util.Log;
-import android.view.View;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.google.gson.Gson;
-import com.star4droid.star2d.Items.*;
-import com.star4droid.star2d.Utils;
-import com.star4droid.template.Utils.ItemScript;
+import com.star4droid.star2d.editor.Utils;
+import com.star4droid.star2d.editor.items.EditorItem;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,7 +18,6 @@ import java.util.HashMap;
 public class PropertySet<K, V> extends HashMap<String,Object> {
 	private Matrix4 bodyMatrix=new Matrix4(),finalMatrix=new Matrix4();
 	private PropertySet parent;
-	private ItemScript script;
 	private boolean printed=false;
 	private ArrayList<PropertySet> childs=new ArrayList<>();
 	public PropertySet(){
@@ -29,7 +27,6 @@ public class PropertySet<K, V> extends HashMap<String,Object> {
 	public static PropertySet<String,Object> getFrom(String s){
 		PropertySet<String,Object> propertySet= Utils.getProperty(s);
 		propertySet.init();
-		propertySet.updateMatrixToCurrent();
 		return propertySet;
 	}
 	
@@ -45,15 +42,6 @@ public class PropertySet<K, V> extends HashMap<String,Object> {
 		if(finalMatrix==null) finalMatrix = new Matrix4();
 		if(bodyMatrix==null) bodyMatrix = new Matrix4();
 	}
-	
-	public void setScript(ItemScript itemScript){
-		script = itemScript;
-	}
-	
-	public ItemScript getScript(){
-		return script;
-	}
-	
 	
 	public boolean setParent(PropertySet propertySet){
 		if(propertySet==parent) return true;
@@ -71,14 +59,12 @@ public class PropertySet<K, V> extends HashMap<String,Object> {
 					}
 				}
 			parent=null;
-			updateMatrixToCurrent();
 			return true;
 		}
 		if(getString("name").equals(propertySet.getString("name"))) return false;
 		if(containsChild(propertySet)) return false;
 		propertySet.childs.add(this);
 		parent = propertySet;
-		updateMatrixToCurrent();
 		return true;
 	}
 	
@@ -98,53 +84,20 @@ public class PropertySet<K, V> extends HashMap<String,Object> {
 	public void setPosition(float x,float y){
 		put("x",x);
 		put("y",y);
-		updateMatrixToCurrent();
-		updateChildsMatrix();
 	}
 	
-	public static PropertySet<String,Object> getDefualt(View view,String addition){
-		
-		String add=(addition==""?"":(","+Utils.readAssetFile("items/"+addition,view.getContext())));
-		return getFrom(Utils.readAssetFile("items/default.json",view.getContext()).replace("--other--",add).replace("ItemName",((Editor)(view.getParent())).getName(view))).updateMatrixToCurrent();
-	}
-	
-	public PropertySet<K,V> updateMatrix(){
-		if(parent==null){
-			updateChildsMatrix();
-			return this;
+	public static PropertySet<String,Object> getDefualt(Actor actor,String addition){
+		String add=(addition==""?"":(","+Gdx.files.internal("items/"+addition).readString())),
+				json = Gdx.files.internal("items/default.json").readString();
+		try {
+			json = json.replace("--other--",add).replace("ItemName",actor.getName());
+		} catch(Exception e){
+			Gdx.files.external("logs/getDefault.error.txt").writeString(Utils.getStackTraceString(e)+"\n",true);
+			e.printStackTrace();
+			return null;
 		}
-		finalMatrix.set(bodyMatrix.cpy().mul(parent.finalMatrix));
-		Vector3 pos=new Vector3();
-		finalMatrix.getTranslation(pos);
-		//Quaternion quat=new Quaternion();
-		put("x",pos.x);
-		put("y",pos.y);
-		//put("rotation",finalMatrix.getRotation(quat).getRoll());
-		updateChildsMatrix();
-		return this;
+		return getFrom(json);
 	}
-	
-	public void updateChildsMatrix(){
-		for(PropertySet<String,Object> propertySet:childs)
-			propertySet.updateMatrix();
-	}
-	
-	public PropertySet<K,V> updateMatrixToCurrent(){
-		updateMatrixToCurrent(true);
-		return this;
-	}
-	
-	public PropertySet<K,V> updateMatrixToCurrent(boolean updateChilds){
-		if(!(containsKey("x")&&containsKey("y"))) return this;
-		finalMatrix.set(new Vector3(getFloat("x"),getFloat("y"),0),new Quaternion()/*.setEulerAngles(0,0,getFloat("rotation"))*/);
-		if(parent==null)
-			bodyMatrix.idt();
-				else bodyMatrix.set(parent.finalMatrix.cpy().inv().mul(finalMatrix));
-		if(updateChilds) updateChildsMatrix();
-		return this;
-	}
-	
-	
 	
 	@Override
 	public Object get(Object key) {
@@ -167,9 +120,9 @@ public class PropertySet<K, V> extends HashMap<String,Object> {
 		return 0;
 	}
 	
-	public int getColor(String key){
+	public Color getColor(String key){
 		try {
-			return Color.parseColor(getString(key));
+			return Color.valueOf(getString(key));
 		} catch (Exception ex){
 		    // Log.e(Utils.error_tag,"getting key error "+key+" :\n"+Log.getStackTraceString(ex));
 		    // if(!printed) {
@@ -177,7 +130,7 @@ public class PropertySet<K, V> extends HashMap<String,Object> {
 			    // printed=true;
 			// }
 		}
-			return Color.WHITE;
+		return Color.WHITE;
 	}
 	
 	public int getInt(String key){
@@ -190,7 +143,7 @@ public class PropertySet<K, V> extends HashMap<String,Object> {
 			    // printed=true;
 			// }
 		}
-			return 0;
+		return 0;
 	}
 	
 	@Override
@@ -217,10 +170,10 @@ public class PropertySet<K, V> extends HashMap<String,Object> {
 	}
 	
 	public static PropertySet<String,Object> empty = new PropertySet<>();
-	public static PropertySet<String,Object> getPropertySet(View view){
-		if(view==null) return empty;
+	public static PropertySet<String,Object> getPropertySet(Actor actor){
+		if(actor==null || !(actor instanceof EditorItem)) return empty;
 		try {
-			PropertySet<String,Object> propertySet =((EditorItem)view).getPropertySet();
+			PropertySet<String,Object> propertySet =((EditorItem)actor).getPropertySet();
 			return propertySet;
 		} catch (Exception ex){}
 		return empty;

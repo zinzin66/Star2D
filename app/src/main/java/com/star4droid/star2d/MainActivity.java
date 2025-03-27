398c8fa38600d53b0b2c8ed7404e97f791bf29a8
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-03-27 21:38:17 +0200

    Update ./com/star4droid/star2d/MainActivity.java

--- app/src/main/java/com/star4droid/star2d/MainActivity.java
+++ app/src/main/java/com/star4droid/star2d/MainActivity.java
@@ -1,13 +1,18 @@
 package com.star4droid.star2d;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 
 import android.os.Bundle;
 import com.star4droid.star2d.Helpers.EngineSettings;
 import com.star4droid.star2d.evo.R;
 import java.util.Timer;
 import java.util.TimerTask;
+import androidx.appcompat.app.AlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +25,47 @@ public class MainActivity extends AppCompatActivity {
 		new Timer().schedule(new TimerTask(){
 			@Override
 			public void run() {
-				Intent i = new Intent();
-				i.setClass(MainActivity.this,com.star4droid.star2d.ProjectsActivity.class);
-				startActivity(i);
-				finish();
+			    if(checkPerms(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE/*, Manifest.permission.ACCESS_MEDIA_LOCATION*/)){
+				    open();
+				} 
 			}
 		},3000);
 		 
+    }
+    
+    private void open(){
+        Intent i = new Intent();
+		i.setClass(MainActivity.this,com.star4droid.star2d.EditorActivity.class);
+		startActivity(i);
+		finish();
+    }
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000) {
+            open();
+        }
+    }
+    
+    public boolean checkPerms(final String... perms) {
+		for(String i : perms)
+        if (ContextCompat.checkSelfPermission(this, i) == PackageManager.PERMISSION_DENIED) {
+            final AlertDialog dialog = new AlertDialog.Builder(this).create();
+            dialog.setTitle("Warning !");
+            dialog.setMessage("Give the the necessary permissions...");
+            dialog.setButton(
+                    AlertDialog.BUTTON_POSITIVE,
+                    "Ok",
+                    (arg0, arg1) -> {
+                        ActivityCompat.requestPermissions(
+                                MainActivity.this, perms, 1000);
+                        dialog.dismiss();
+                    });
+            dialog.setCancelable(false);
+            dialog.show();
+            return false;
+        }
+        return true;
     }
 }

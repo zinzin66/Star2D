ccefc2c530ea75d0ed3b2ed7092e58a526549ca3
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-04-08 17:41:03 +0200

    Delete app/src/main/java directory

--- app/src/main/java/com/star4droid/star2d/JointInputs/JointInput.java
+++
@@ -1,8 +0,0 @@
-package com.star4droid.star2d.JointInputs;
-
-public interface JointInput {
-	public String getValue();
-	public void setValue(Object object);
-	public String getName();
-	public String getCode();
-}
--- app/src/main/java/com/star4droid/star2d/editor/ui/sub/inputs/InputField.java
+++
@@ -1,9 +0,0 @@
-package com.star4droid.star2d.editor.ui.sub.inputs;
-
-public interface InputField {
-	public void setNameText(String name);
-	public void setValue(String value);
-	public String getValue();
-	public String getFieldName();
-	public void setOnChange(Runnable runnable);
-}

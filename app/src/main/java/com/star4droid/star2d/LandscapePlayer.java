43f4774a9635ad3dd395a7e66474a2f81007f13f
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-04-09 09:57:28 +0200

    Update ./com/star4droid/star2d/LandscapePlayer.java

---
+++ app/src/main/java/com/star4droid/star2d/LandscapePlayer.java
@@ -0,0 +1,11 @@
+package com.star4droid.star2d;
+
+import com.star4droid.template.MainActivity;
+import android.os.Bundle;
+
+public class LandscapePlayer extends MainActivity {
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+	}
+}

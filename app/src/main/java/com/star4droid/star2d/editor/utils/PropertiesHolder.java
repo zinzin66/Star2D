ff1363a90779f1010367e6cbdd7593a463f9d704
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-04-09 09:52:43 +0200

    Update ./com/star4droid/star2d/editor/utils/PropertiesHolder.java

---
+++ app/src/main/java/com/star4droid/star2d/editor/utils/PropertiesHolder.java
@@ -0,0 +1,8 @@
+package com.star4droid.star2d.editor.utils;
+
+// useless 
+public class PropertiesHolder {
+	public PropertiesHolder(){}
+	public void updateProperties(){}
+	private void updateInternal(){}
+}

98052549710b5087a2da4d76cd3efc8d12d55ff8
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-04-21 16:15:55 +0200

    Update ./com/star4droid/star2d/Adapters/UpdateChecker.java

--- app/src/main/java/com/star4droid/star2d/Adapters/UpdateChecker.java
+++ app/src/main/java/com/star4droid/star2d/Adapters/UpdateChecker.java
@@ -1,14 +1,9 @@
 package com.star4droid.star2d.Adapters;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Handler;
-import android.os.Looper;
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.star4droid.star2d.evo.R;
-
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.star4droid.star2d.editor.TestApp;
+import com.star4droid.star2d.editor.ui.sub.ConfirmDialog;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -19,19 +14,20 @@ import java.util.concurrent.Executors;
 public class UpdateChecker {
 
     private static final String CURRENT_VERSION = "0.1.6";
-    private static final String CHECK_UPDATE_URL = "https://raw.githubusercontent.com/star4droid/Star2D/refs/heads/master/assets/latest.version"; // Replace with your URL
-    private static final String UPDATE_PAGE_URL = "https://github.com/abodinagdat16/Star2D/releases/"; // Replace with your update page URL
+    private static final String CHECK_UPDATE_URL = "https://raw.githubusercontent.com/star4droid/Star2D/refs/heads/master/assets/latest.version"; 
+    private static final String UPDATE_PAGE_URL = "https://github.com/star4droid/Star2D/releases/"; 
 
-    public static void checkForUpdate(Context context) {
+    public static void checkForUpdate(TestApp testApp) {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Handler mainHandler = new Handler(Looper.getMainLooper());
-
+        
         executorService.execute(() -> {
             String latestVersion = fetchLatestVersion();
-            com.badlogic.gdx.Gdx.files.external("logs/update.txt").writeString("version : "+latestVersion+"\n",true);
-            mainHandler.post(() -> {
+            if(latestVersion!=null)
+				com.badlogic.gdx.Gdx.files.external("logs/update/latest.txt").writeString("version : "+latestVersion+"\n",false);
+            Gdx.app.postRunnable(() -> {
                 if (latestVersion != null && !latestVersion.equals(CURRENT_VERSION)) {
-                    showUpdateDialog(context);
+                    Gdx.files.external("logs/update/"+CURRENT_VERSION+".txt").writeString(latestVersion,false);
+					showUpdateDialog(testApp);
                 }
             });
         });
@@ -39,6 +35,12 @@ public class UpdateChecker {
 
     private static String fetchLatestVersion() {
         String response = null;
+		FileHandle folder = Gdx.files.external("logs/update");
+		FileHandle version = folder.child(CURRENT_VERSION+".txt");
+		if(folder.exists() && folder.isDirectory() && version.exists() && !version.readString().equals("")){
+			return version.readString();
+		} else if(folder.exists() && folder.isDirectory())
+			folder.deleteDirectory();
         try {
             URL url = new URL(CHECK_UPDATE_URL);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -57,19 +59,26 @@ public class UpdateChecker {
                 reader.close();
                 response = result.toString().trim();
             } else {
-                com.badlogic.gdx.Gdx.files.external("logs/update.txt").writeString("response not ok\n",true);
+                com.badlogic.gdx.Gdx.files.external("logs/update/log.txt").writeString("response not ok\n",true);
             }
             connection.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
-            com.badlogic.gdx.Gdx.files.external("logs/update.txt").writeString("Error : "+e.toString()+"\n",true);
+			if(!e.toString().toLowerCase().contains("no address"))
+            	com.badlogic.gdx.Gdx.files.external("logs/update/log.txt").writeString("Error : "+e.toString()+"\n",true);
            // throw new RuntimeException(e.toString());
         }
         return response;
     }
 
-    private static void showUpdateDialog(Context context) {
-        new MaterialAlertDialogBuilder(context)
+    private static void showUpdateDialog(TestApp app) {
+		if(app.getProjectsStage()!=null)
+		new ConfirmDialog("Update Available","Update Available!\nDo you want to update ?",ok->{
+			if(ok){
+				Gdx.net.openURI(UPDATE_PAGE_URL);
+			}
+		}).show(app.getProjectsStage());
+		/*new MaterialAlertDialogBuilder(context)
                 .setTitle(context.getString(R.string.update_needed))
                 .setMessage(context.getString(R.string.update_needed))
                 .setCancelable(false)
@@ -79,5 +88,6 @@ public class UpdateChecker {
                     context.startActivity(browserIntent);
                 })
                 .show();
+		*/
     }
 }

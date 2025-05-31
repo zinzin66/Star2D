4b59ee1d49a11b6afde3bc70f650e484f89d125c
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-05-31 16:21:01 +0200

    Update ./example1/java/com/star4droid/Game/SceneScript/scene1Script.java

---
+++ app/src/main/assets/files/examples/example1/java/com/star4droid/Game/SceneScript/scene1Script.java
@@ -0,0 +1,24 @@
+package com.star4droid.game.SceneScript;
+import com.star4droid.template.Utils.SceneScript;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.Gdx;
+import com.star4droid.template.Items.*;
+import com.star4droid.template.Utils.PlayerItem;
+/*
+    Hi Coder :)
+    & Thanks for using Star2D Evolution 
+*/
+public class scene1Script extends SceneScript {
+    public void create(){
+        //getStage().debugBox2d = true;
+    }
+	public void draw(){
+	    
+	}
+	public void pause(){
+	    
+	}
+	public void resume(){
+	    
+	}
+}

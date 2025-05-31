b8732ea2d3f55f784f92794aba62d7adbaf9161a
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-05-31 16:30:45 +0200

    Update ./StarValley/java/com/star4droid/Game/Scripts/scene1/Progress1Script.java

---
+++ app/src/main/assets/files/examples/StarValley/java/com/star4droid/Game/Scripts/scene1/Progress1Script.java
@@ -0,0 +1,40 @@
+package com.star4droid.Game.Scripts.scene1;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.star4droid.template.Utils.PlayerItem;
+import com.star4droid.template.Utils.ItemScript;
+import com.star4droid.template.Items.*;
+import com.star4droid.Game.*;
+import com.star4droid.Game.GdxTime;
+
+public class Progress1Script extends ItemScript {
+/*
+    Hi coder... :)
+    the engine still in it's beta and you may face so many bugs,
+    so please contact us without hesitation...
+*/
+    public void onClick(){
+        
+    }
+    public void onTouchStart(InputEvent event){
+        
+    }
+    public void onTouchEnd(InputEvent event){
+        
+    }
+    public void onBodyCreated(){
+        
+    }
+    public void onBodyUpdate(){
+        GdxTime time = GdxTime.getTime(1440*2);
+        getPlayerItem().setProgress(time.getHour()*60 + time.getMin());
+    }
+    public void onCollisionBegin(PlayerItem other){
+        
+    }
+    public void onCollisionEnd(PlayerItem other){
+        
+    }
+    
+}

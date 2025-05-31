0c7e681db4959f288790c1e8f55377bf3e2debfb
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-05-31 16:25:35 +0200

    Update ./platformer/java/com/star4droid/Game/Scripts/scene1/coinScript.java

---
+++ app/src/main/assets/files/examples/platformer/java/com/star4droid/Game/Scripts/scene1/coinScript.java
@@ -0,0 +1,38 @@
+package com.star4droid.Game.Scripts.scene1;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.star4droid.template.Utils.PlayerItem;
+import com.star4droid.template.Utils.ItemScript;
+import com.star4droid.template.Items.*;
+
+public class coinScript extends ItemScript {
+/*
+    Hi coder... :)
+    the engine still in it's beta and you may face so many bugs,
+    so please contact us without hesitation...
+*/
+    
+    public void onClick(){
+        
+    }
+    public void onTouchStart(InputEvent event){
+        
+    }
+    public void onTouchEnd(InputEvent event){
+        
+    }
+    public void onBodyCreated(){
+        
+    }
+    public void onBodyUpdate(){
+        
+    }
+    public void onCollisionBegin(PlayerItem other){
+        
+    }
+    public void onCollisionEnd(PlayerItem other){
+        
+    }
+    
+}

2a79520b925c8f05eb47c56d9f4f749821de00de
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-05-31 16:31:37 +0200

    Update ./StarValley/java/com/star4droid/star2d/player/PlayerItem.java

---
+++ app/src/main/assets/files/examples/StarValley/java/com/star4droid/star2d/player/PlayerItem.java
@@ -0,0 +1,55 @@
+package com.star4droid.template.Utils;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.star4droid.star2d.ElementDefs.ElementEvent;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.Color;
+
+public interface PlayerItem {
+	public void update();
+	public Body getBody();
+	public void scaleEffect(float sx,float sy);
+	public void setTint(Color color);
+	public void setTint(String hexColor);
+	public ParticleEffect getParticleEffect();
+	public String getName();
+	public void setActorX(float x);
+	public void setActorY(float y);
+	public String getParentName();
+	public PlayerItem getChild(String child);
+	public void setScript(ItemScript script);
+	public <T extends ItemScript> T getScript();
+	public ChildsHolder getChildsHolder();
+	public void setParent(PlayerItem item);
+	public void addChild(PlayerItem item);
+	public PropertySet<String,Object> getProperties();
+	public PlayerItem getClone(String newName);
+	public void setItemText(String text);
+    public void setProgress(int progress);
+	public int getProgress();
+    public void setMax(int max);
+	public float getJoyStickX();
+	public float getJoyStickY();
+	public double getAngle();
+	public double getPower();
+	public double getAngleDegrees();
+	public void destroy();
+	public float getBodyX();
+	public float getBodyY();
+	public float distToPoint(float x,float y);
+	public float getActorX();
+	public float getActorY();
+	public float distTo(PlayerItem item);
+	public boolean isExsits(String file);
+	public Actor getActor();
+	public void setImage(FileHandle fileHandle);
+	public void setImage(Texture texture);
+	public void setAnimation(String name);
+	public void setAnimation(Animation animation);
+	public void removeAnimation();
+	public ElementEvent getElementEvents();
+}

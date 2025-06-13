47a0709c51406cc0d257c5dfd3a918a50950a419
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-06-13 23:31:29 +0200

    Update ./star2d/editor/ui/sub/inputs/InputField.java

--- app/src/main/java/com/star4droid/star2d/editor/ui/sub/inputs/InputField.java
+++ app/src/main/java/com/star4droid/star2d/editor/ui/sub/inputs/InputField.java
@@ -1,9 +1,20 @@
 package com.star4droid.star2d.editor.ui.sub.inputs;
 
+import com.star4droid.star2d.editor.TestApp;
+import com.star4droid.star2d.editor.utils.Lang;
+
 public interface InputField {
 	public void setNameText(String name);
 	public void setValue(String value);
 	public String getValue();
 	public String getFieldName();
 	public void setOnChange(Runnable runnable);
+	default public void showDetails(){
+		TestApp app = TestApp.getCurrentApp();
+		String details = Lang.getTrans(getFieldName()+"Details");
+		if(!details.equals(getFieldName()+"Details")){
+			app.getSimpleNote().setMessage(details)
+					.show(app.getUiStage());
+		}
+	}
 }

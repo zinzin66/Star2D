68f266f5664d92ca91821e41c6bdf66dfbeda9e4
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-06-13 23:31:39 +0200

    Update ./star2d/editor/ui/sub/inputs/CheckInput.java

--- app/src/main/java/com/star4droid/star2d/editor/ui/sub/inputs/CheckInput.java
+++ app/src/main/java/com/star4droid/star2d/editor/ui/sub/inputs/CheckInput.java
@@ -1,15 +1,19 @@
 package com.star4droid.star2d.editor.ui.sub.inputs;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.widget.VisCheckBox;
 import com.kotcrab.vis.ui.widget.VisTable;
 import com.kotcrab.vis.ui.widget.VisTextButton;
+import static com.star4droid.star2d.editor.utils.Lang.*;
 
 public class CheckInput extends VisTable implements InputField{
 	VisTextButton name;
 	VisCheckBox checkBox;
+	private String fieldName;
 	Runnable onChange;
 	ChangeListener changeListener;
 	boolean disableListener = false;
@@ -20,10 +24,17 @@ public class CheckInput extends VisTable implements InputField{
 		checkBox = new VisCheckBox("");
 		name.setBackground(VisUI.getSkin().getDrawable("separator"));
 		checkBox.setBackground(VisUI.getSkin().getDrawable("separator"));
-		add().width(8);
-		add(name).minWidth(100).growX();
-		add().width(8);
-		add(checkBox).minWidth(80).padRight(8);
+		if(isRTL()){
+			add().width(8);
+			add(checkBox).minWidth(80);
+			add().width(8);
+			add(name).padRight(8).growX();
+		} else {
+			add().width(8);
+			add(name).minWidth(100).growX();
+			add().width(8);
+			add(checkBox).minWidth(80).padRight(8);
+		}
 		changeListener = new ChangeListener() {
 			@Override
 			public void changed (ChangeEvent event, Actor actor) {
@@ -32,14 +43,21 @@ public class CheckInput extends VisTable implements InputField{
 			}
 		};
 		checkBox.addListener(changeListener);
+		name.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+				showDetails();
+			}
+		});
 	}
 	@Override
 	public String getFieldName() {
-		return name.getText().toString();
+		return fieldName;
 	}
 	@Override
 	public void setNameText(String nm) {
-		name.setText(nm);
+		fieldName = nm;
+		name.setText(getTrans(nm));
 		//checkBox.setText(nm);
 	}
 	

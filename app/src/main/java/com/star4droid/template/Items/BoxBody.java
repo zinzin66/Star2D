8761f7bfe634f1ba1076e5aedc0c3985a1e0664d
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-09-15 13:28:22 +0200

    Update ./com/star4droid/template/Items/BoxBody.java

--- app/src/main/java/com/star4droid/template/Items/BoxBody.java
+++ app/src/main/java/com/star4droid/template/Items/BoxBody.java
@@ -3,7 +3,6 @@ package com.star4droid.template.Items;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;

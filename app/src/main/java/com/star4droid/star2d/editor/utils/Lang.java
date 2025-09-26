da2c31504997068ebdd2b2a3b6af64ff00ab89ff
Zinzin <candy.thoury@gmail.com>
2025-09-26 23:10:27 +0200

    Update Lang.java

--- app/src/main/java/com/star4droid/star2d/editor/utils/Lang.java
+++ app/src/main/java/com/star4droid/star2d/editor/utils/Lang.java
@@ -7,36 +7,60 @@ import java.util.HashMap;
 import java.util.Locale;
 
 public class Lang {
-	/*
-		Language Manager Class...
-	*/
-	private static I18NBundle currentLang = null;
-	private static HashMap<String,I18NBundle> map = new HashMap<>();
-	public static void loadTrans(String lang){
-		String key = lang.equals("ar") ? lang : "en";
-		if(map.containsKey(key)){
-			currentLang = map.get(key);
-			return;
-		}
-		currentLang = I18NBundle.createBundle(Gdx.files.internal("i18n/"+(lang.equals("ar") ? "strings_ar_SD" : "strings_en_GB")),lang.equals("ar") ? new Locale("ar","SD") : Locale.UK);
-		map.put(key,currentLang);
-	}
-	
-	public static String getTrans(String name){
-		try {
-			String firstChar = String.valueOf(name.charAt(0));
-    	    boolean needModify = (firstChar.equals(firstChar.toUpperCase()) && !firstChar.equals("")) || name.contains(" ");
-    	    if(!needModify)
-    	        return currentLang.get(name);
-    		String newName = name.replace(" ","");
-    		newName = String.valueOf(newName.charAt(0)).toLowerCase() + newName.substring(1,newName.length());
-			return currentLang.get(newName);
-		} catch(Exception | Error e){
-			return name;
-		}
-	}
-	
-	public static boolean isRTL(){
-		return TestApp.getCurrentApp().preferences.getString("lang","en").equals("ar");
-	}
+    /*
+        Language Manager Class...
+    */
+    private static I18NBundle currentLang = null;
+    private static HashMap<String,I18NBundle> map = new HashMap<>();
+
+    public static void loadTrans(String lang){
+        String fileName;
+        Locale locale;
+
+        switch (lang) {
+            case "ar":
+                fileName = "strings_ar_SD";
+                locale = new Locale("ar", "SD");
+                break;
+            case "fr":
+                fileName = "strings_fr";
+                locale = Locale.FRENCH;
+                break;
+            case "br":
+                fileName = "strings_br";
+                locale = new Locale("pt", "BR");
+                break;
+            default:
+                fileName = "strings_en_GB";
+                locale = Locale.UK;
+                break;
+        }
+
+        if(map.containsKey(lang)){
+            currentLang = map.get(lang);
+            return;
+        }
+
+        currentLang = I18NBundle.createBundle(Gdx.files.internal("i18n/" + fileName), locale);
+        map.put(lang, currentLang);
+    }
+
+    public static String getTrans(String name){
+        try {
+            String firstChar = String.valueOf(name.charAt(0));
+            boolean needModify = (firstChar.equals(firstChar.toUpperCase()) && !firstChar.equals("")) || name.contains(" ");
+            if(!needModify)
+                return currentLang.get(name);
+
+            String newName = name.replace(" ","");
+            newName = String.valueOf(newName.charAt(0)).toLowerCase() + newName.substring(1,newName.length());
+            return currentLang.get(newName);
+        } catch(Exception | Error e){
+            return name;
+        }
+    }
+
+    public static boolean isRTL(){
+        return TestApp.getCurrentApp().preferences.getString("lang","en").equals("ar");
+    }
 }
